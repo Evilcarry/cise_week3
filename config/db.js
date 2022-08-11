1969a937f7a80bdd9dbcd2b878d55d0fd1f76fe3
Evilcarry <benjhithaxx@gmail.com>
2022-08-11 15:54:27 +1200

    changes

--- config/db.js
+++ config/db.js
@@ -2,20 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      db,
-      {
-        useNewUrlParser: true
-      }
-    );
-
-    console.log('MongoDB is Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
+mongoose.connect(process.env.MONGODB_URI || db, {
+  useNewUrlParser: true
+});
 
 module.exports = connectDB;
