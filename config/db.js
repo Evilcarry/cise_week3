4e647f3a33c65fce0d2c7b91dcb328b3576e673d
Evilcarry <benjhithaxx@gmail.com>
2022-08-11 16:07:15 +1200

    xd

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-mongoose.connect(process.env.MONGODB_URI || db, {
-  useNewUrlParser: true
-});
+const connectDB = async() =>{
+  mongoose.connect(process.env.MONGODB_URI || db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
 
 module.exports = connectDB;
