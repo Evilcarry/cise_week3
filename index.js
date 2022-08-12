599c170a8321d1b8b5813cc96ff0c904a09dcb1e
Evilcarry <benjhithaxx@gmail.com>
2022-08-12 13:51:48 +1200

    maybe

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-// app.js
-
 const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
-// routes
-const books = require('./routes/api/books.js')(app);
+//models
+require("./models/book");
 
-// Connect Database
-connectDB();
+mongoose
+    .connect(
+        process.env.MONGODB_CONNECTION_STRING,
+        {
+            userNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+        )
+    .then(() => console.log("MongoDB has been connected"))
+    .catch((err) => console.log(err));
 
-// cors
-app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// routes
+require('./routes/api/books.js')(app);
+
 //test
-const path = require("path");
-const { response } = require('express');
 const port = process.env.PORT || 8082;
+
+const path = require("path");
+
+const { response } = require('express');
 
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 
 app.get("*", function(req, res) {
     response.sendFile(path.resolve(__dirname, "./frontend/build", index.html));
 })
-
-//app.use(express.json({ extended: false }));
-//app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
-//app.use('/api/books', books);
-
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('./frontend/build'))
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
