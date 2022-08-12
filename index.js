41a8d6ee9ab396608d584b6cf59b009aa5d79bbb
Evilcarry <benjhithaxx@gmail.com>
2022-08-12 12:27:56 +1200

    commit message

--- index.js
+++ index.js
@@ -16,17 +16,26 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
-app.use('/api/books', books);
-
+//test
+const path = require("path");
+const { response } = require('express');
 const port = process.env.PORT || 8082;
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
-}
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+
+app.get("*", function(req, res) {
+    response.sendFile(path.resolve(__dirname, "./frontend/build", index.html));
+})
+
+//app.use(express.json({ extended: false }));
+//app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+//app.use('/api/books', books);
+
+
+//if (process.env.NODE_ENV === 'production'){
+//    app.use(express.static('./frontend/build'))
+//}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
